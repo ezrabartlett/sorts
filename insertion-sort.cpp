fad1b97f3f062571e4267ab0fe9f7d76adc26bec
ezrabartlett <ezrabartlett@linux2.cs.tamu.edu>
2017-02-19 17:14:13 -0600

    implementation of the simple insertion sort

--- insertion-sort.cpp
+++ insertion-sort.cpp
@@ -7,11 +7,20 @@
 //============================================================================
 
 #include "sort.h"
+#include "iostream"
 
 void
 InsertionSort::sort(int A[], int size)				// main entry point
 {
-  /* Complete this function with the implementation of insertion sort algorithm 
-  Record number of comparisons in variable num_cmps of class Sort
-  */
+  for(int i = 0; i< size; i++)
+  {
+	int j = i, temp;
+	while(A[j]<A[j-1])
+	{
+		temp = A[j-1];
+		A[j-1]=A[j];
+		A[j]=temp;
+		j--;
+	}	
+  }
 }
