d93a224801977f130c8c1b03d0c8542f8c9fb3a3
ezrabartlett <ezrabartlett@linux2.cs.tamu.edu>
2017-02-19 17:46:40 -0600

    Implementation of the selection-sort

--- selection-sort.cpp
+++ selection-sort.cpp
@@ -15,4 +15,17 @@ SelectionSort::sort(int A[], int size)				// main entry point
   /* Complete this function with the implementation of selection sort algorithm 
   Record number of comparisons in variable num_cmps of class Sort
   */
+  for(int i = size-1; i>=0 ; i--)
+  {
+	  int index = 0;
+	  for(int j = 0; j<=i; j++)
+		{
+			if(A[j]>A[index]){
+				index = j;
+			}
+	  }
+	  int temp = A[i];
+	  A[i] = A[index];
+	  A[index]=temp;
+  }
 }
