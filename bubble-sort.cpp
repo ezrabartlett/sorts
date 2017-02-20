d7544a95abeec2fb090815b2f27f9c8a3ce98cb8
ezrabartlett <ezrabartlett@linux2.cs.tamu.edu>
2017-02-19 18:10:59 -0600

    bubble sort implementation

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -14,4 +14,19 @@ BubbleSort::sort(int A[], int size)			// main entry point
   /* Complete this function with the implementation of bubble sort algorithm 
      Record number of comparisons in variable num_cmps of class Sort
   */
+	bool flag = 1, iterations = 1;
+	while(flag)
+	{
+		flag = 0;
+		for(int i = 0; i<size-iterations; i++)
+		{
+			if(A[i]>A[i+1])
+			{
+				int temp = A[i+1];
+				A[i+1] = A[i];
+				A[i] = temp;
+				flag = 1;
+			}	
+		}	
+	}	
 }
