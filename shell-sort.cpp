4fd0b4ccc3149d1f24fe17623b7a5b188e6d2d72
ezrabartlett <ezrabartlett@linux2.cs.tamu.edu>
2017-02-19 19:05:45 -0600

    added shell-sort code

--- shell-sort.cpp
+++ shell-sort.cpp
@@ -15,4 +15,30 @@ ShellSort::sort(int A[], int size)
   /* Complete this function with the implementation of shell sort algorithm 
   Record number of comparisons in variable num_cmps of class Sort
   */
+	for(int i = size/2; i>= 1; i/=2)
+	{
+		for(int j=0;j<=size-i-1; j++)
+		{
+			if(A[j]>A[j+i])
+			{
+				int n = j+i;
+				bool flag = 1;
+				while(flag)
+				{
+					flag = 0;
+					while(n>=i)
+					{
+						if(A[n-i]>A[n])
+						{
+							int temp = A[n];
+							A[n] = A[n-i];
+							A[n-i]= temp;
+							flag = 1;
+						}	
+						n-=i;
+					}
+				}	
+			}	
+		}	
+	}	
 }
